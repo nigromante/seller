c378eef9a1f7d89a8363cda405434f45d229edf6
Nigromante <nigromante@github.com>
2026-03-07 12:30:00 -0300

    upgrade controller calls

--- src/main/java/com/nigromante/seller/infrastructure/controllers/OrderController.java
+++ src/main/java/com/nigromante/seller/infrastructure/controllers/OrderController.java
@@ -1,5 +1,7 @@
 package com.nigromante.seller.infrastructure.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,11 @@ import java.util.List;
 import com.nigromante.seller.application.services.*;
 
 @RestController
+@RequestMapping( value = "order" )
 public class OrderController {
     
-  @GetMapping("/order")
+
+  @PostMapping( value="getall")
   public String list() {
       
       OrderService service = new OrderServiceImpl(); 
@@ -19,7 +23,18 @@ public class OrderController {
     return "saludos desde (GET /order): " + this.getClass().getName();
   } 
 
-  @PostMapping("/order")
+
+  @PostMapping( value="find")
+  public String findById( @RequestParam String orderId ) {
+      
+      OrderService service = new OrderServiceImpl(); 
+        List<String> result = service.list();
+      
+    return "saludos desde (GET /find): " + this.getClass().getName() + "  orderId: " + orderId ;
+  } 
+
+
+  @PostMapping( value = "create" )
   public String create() {
       
         OrderService service = new OrderServiceImpl(); 
