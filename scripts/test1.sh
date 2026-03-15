dc51d9a2c30f4da178e69e21dbda36044c054f3d
Nigromante <nigromante@github.com>
2026-03-14 22:45:57 -0300

    test multiline for better read

--- scripts/test1.sh
+++ scripts/test1.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-curl -s --request POST --header "Content-Type: application/json"  --data '{ "customerId" : "createorder", "orderId" : "orderone" }'    http://localhost:8080/order/create |  json_pp 
+curl -s --request POST \
+    --header "Content-Type: application/json" \
+    --data '{ "customerId" : "createorder", "orderId" : "orderone" }' \
+    http://localhost:8080/order/create \
+    |  json_pp \
+/ 
 
   
